27e673dbb6fca9143318d1a18d1f6b9d3b0164b5
bsereyreach-lang <bsereyreach@gmail.com>
2026-06-06 11:33:29 +0000

    eeee

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("ready", () => {
+  console.log("Bot is ONLINE");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  console.log("Message:", message.content);
+});
+
+client.login(process.env.TOKEN);
