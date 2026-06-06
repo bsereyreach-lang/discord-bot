15a57dd91f5bca93ea76f86e3fc308cb884c8f93
bsereyreach-lang <bsereyreach@gmail.com>
2026-06-06 11:54:42 +0000

    e

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
 
+const app = express();
+app.use(express.json());
+
+// STORE LAST MESSAGE
+let latestMessage = "";
+
+// DISCORD BOT
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -12,9 +20,23 @@ client.on("ready", () => {
   console.log("BOT ONLINE");
 });
 
+// READ DISCORD MESSAGE
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-  console.log(message.content);
+
+  latestMessage = message.content;
+  console.log("Discord:", latestMessage);
 });
 
+// API FOR ROBLOX
+app.get("/message", (req, res) => {
+  res.json({ message: latestMessage });
+});
+
+// START SERVER
+app.listen(3000, () => {
+  console.log("API RUNNING");
+});
+
+// LOGIN BOT
 client.login(process.env.TOKEN);
